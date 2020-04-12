dca0baf5cd65997211d0cada41f00d8900ab8bec
Karl Jiang <karl@figma.com>
2020-04-12 10:35:08 -0700

    add files

---
+++ client/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mode: "production",
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
