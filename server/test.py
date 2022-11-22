da144d89ed25e61c0f7fafbbda3d9d4d32df34c4
yangbaechu <abab_1212@naver.com>
2022-11-22 10:36:32 +0900

    test.py에서 환경 실행하기

--- server/test.py
+++ server/test.py
@@ -1,9 +1,17 @@
 from flask import Flask
-#import player_and_game
-#from splendor import GameManager
-import splendor
+from  splendor import *
+from time import sleep
 
-#GM = GameManager("Aircraft")
+GM = GameManager("Aircraft")
 #GM.start_game()
+#game = GM.game
+#game.reset()
+
+#print(game.decks["level1"])
+
+#sleep(5)
+#game.reset()
+#print("reset")
 
 #game.start_game()
+app.run(host='127.0.0.1', port=8000, threaded=True)
